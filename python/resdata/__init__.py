de8442ef49cb77bebc607a2e18928c19e7a44528
Zohar Malamant <ZOM@equinor.com>
2024-04-29 16:20:16 +0200

    Implement pybind11 bindings

--- python/resdata/__init__.py
+++ python/resdata/__init__.py
@@ -43,145 +43,47 @@ warnings.filterwarnings(
 
 from cwrap import Prototype
 
-RD_LEGACY_INSTALLED = not os.path.isdir(
-    os.path.join(os.path.dirname(__file__), ".libs")
-)
-if not RD_LEGACY_INSTALLED:
-    from .version import version as __version__
+from .version import version as __version__
 
-    def get_include():
-        return os.path.join(os.path.dirname(__file__), ".include")
+def dlopen_resdata():
+    import resdata.resdata
 
-    def dlopen_resdata():
-        import ctypes
-        import platform
+    return ctypes.CDLL(resdata._clib.__file__)
 
-        path = os.path.join(os.path.dirname(__file__), ".libs")
-        if platform.system() == "Linux":
-            path = os.path.join(path, "libresdata.so")
-        elif platform.system() == "Darwin":
-            path = os.path.join(path, "libresdata.dylib")
-        elif platform.system() == "Windows":
-            path = os.path.join(os.path.dirname(__file__), ".bin", "libresdata.dll")
-        else:
-            raise NotImplementedError("Invalid platform")
+# Need to keep the function as a global variable so that we don't give C a
+# dangling pointer
+_abort_handler = None
 
-        return ctypes.CDLL(path, ctypes.RTLD_GLOBAL)
+@ct.CFUNCTYPE(None, ct.c_char_p, ct.c_int, ct.c_char_p, ct.c_char_p, ct.c_char_p)
+def _c_abort_handler(filename, lineno, function, message, backtrace):
+    global _abort_handler
+    if not _abort_handler:
+        return
+    _abort_handler(
+        filename.decode(),
+        lineno,
+        function.decode(),
+        message.decode(),
+        backtrace.decode(),
+    )
 
-    # Need to keep the function as a global variable so that we don't give C a
-    # dangling pointer
-    _abort_handler = None
+def set_abort_handler(function):
+    """
+    Set callback function for util_abort, which is called prior to std::abort()
+    """
+    global _abort_handler
+    _abort_handler = function
 
-    @ct.CFUNCTYPE(None, ct.c_char_p, ct.c_int, ct.c_char_p, ct.c_char_p, ct.c_char_p)
-    def _c_abort_handler(filename, lineno, function, message, backtrace):
-        global _abort_handler
-        if not _abort_handler:
-            return
-        _abort_handler(
-            filename.decode(),
-            lineno,
-            function.decode(),
-            message.decode(),
-            backtrace.decode(),
+    ResdataPrototype.lib.util_set_abort_handler(_c_abort_handler)
+
+class ResdataPrototype(Prototype):
+    lib = dlopen_resdata()
+
+    def __init__(self, prototype, bind=True):
+        super(ResdataPrototype, self).__init__(
+            ResdataPrototype.lib, prototype, bind=bind
         )
 
-    def set_abort_handler(function):
-        """
-        Set callback function for util_abort, which is called prior to std::abort()
-        """
-        global _abort_handler
-        _abort_handler = function
-
-        ResdataPrototype.lib.util_set_abort_handler(_c_abort_handler)
-
-    class ResdataPrototype(Prototype):
-        lib = dlopen_resdata()
-
-        def __init__(self, prototype, bind=True):
-            super(ResdataPrototype, self).__init__(
-                ResdataPrototype.lib, prototype, bind=bind
-            )
-
-else:
-    #
-    # If installed via CMake directly (legacy)
-    #
-    from cwrap import load as cwrapload
-
-    try:
-        import ert_site_init
-    except ImportError:
-        pass
-
-    required_version_hex = 0x02070000
-
-    resdata_path = None
-    resdata_so_version = ""
-    __version__ = "0.0.0"
-
-    # 1. Try to load the __resdata_info module; this module has been
-    #    configured by cmake during the build configuration process. The
-    #    module should contain the variable lib_path pointing to the
-    #    directory with shared object files.
-    try:
-        from .__resdata_info import ResdataInfo
-
-        resdata_path = ResdataInfo.lib_path
-        resdata_so_version = ResdataInfo.so_version
-        __version__ = ResdataInfo.__version__
-    except ImportError:
-        pass
-    except AttributeError:
-        pass
-
-    # 2. Using the environment variable RESDATA_LIBRARY_PATH it is possible to
-    #    override the default algorithms. If the RESDATA_LIBRARY_PATH is set
-    #    to a non existing directory a warning will go to stderr and the
-    #    setting will be ignored.
-    env_lib_path = os.getenv("RESDATA_LIBRARY_PATH")
-    if env_lib_path:
-        if os.path.isdir(env_lib_path):
-            ert_lib_path = os.getenv("RESDATA_LIBRARY_PATH")
-        else:
-            sys.stderr.write(
-                "Warning: Environment variable RESDATA_LIBRARY_PATH points to nonexisting directory:%s - ignored"
-                % env_lib_path
-            )
-
-    # Check that the final ert_lib_path setting corresponds to an existing
-    # directory.
-    if resdata_path:
-        if not os.path.isabs(resdata_path):
-            resdata_path = os.path.abspath(
-                os.path.join(os.path.dirname(__file__), resdata_path)
-            )
-
-        if not os.path.isdir(resdata_path):
-            resdata_path = None
-
-    if sys.hexversion < required_version_hex:
-        raise Exception("ERT Python requires Python 2.7.")
-
-    def load(name):
-        try:
-            return cwrapload(name, path=resdata_path, so_version=resdata_so_version)
-        except ImportError:
-            # For pip installs, setup.py puts the shared lib in this directory
-            own_dir = os.path.dirname(os.path.abspath(__file__))
-            return cwrapload(name, path=own_dir, so_version=resdata_so_version)
-
-    class ResdataPrototype(Prototype):
-        lib = load("resdata")
-
-        def __init__(self, prototype, bind=True):
-            super(ResdataPrototype, self).__init__(
-                ResdataPrototype.lib, prototype, bind=bind
-            )
-
-
-#
-# Common
-#
 
 from .rd_type import ResDataType, ResdataTypeEnum
 from .rd_util import (
@@ -191,13 +93,13 @@ from .rd_util import (
     UnitSystem,
     ResdataUtil,
 )
-from .util.util import ResdataVersion, updateAbortSignals
 
-updateAbortSignals()
-
-
-def root():
-    """
-    Will print the filesystem root of the current ert package.
-    """
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+__all__ = [
+    "ResDataType",
+    "ResdataTypeEnum",
+    "FileType",
+    "FileMode",
+    "Phase",
+    "UnitSystem",
+    "ResdataUtil",
+]
--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 import os
-import sys
-
-import setuptools
-import skbuild
-from setuptools_scm import get_version
-
-version = get_version(relative_to=__file__, write_to="python/resdata/version.py")
+from setuptools import find_packages
+from skbuild import setup
 
 
 # Corporate networks tend to be behind a proxy server with their own non-public
@@ -19,97 +14,15 @@ if "CONAN_CACERT_PATH" not in os.environ:
         break
 
 
-with open("README.md") as f:
-    long_description = f.read()
-
-
-def utility_wrappers():
-    """
-    Wrappers around resdata's "application" utilities. These are only supported on
-    Linux at this time so only create the wrapper when on Linux.
-    """
-    if sys.platform != "linux":
-        return []
-
-    return [
-        name + " = resdata.bin:main"
-        for name in (
-            "CF_dump",
-            "convert.x",
-            "rd_pack.x",
-            "rd_unpack.x",
-            "grdecl_grid",
-            "grdecl_test.x",
-            "grid_dump.x",
-            "grid_dump_ascii.x",
-            "grid_info.x",
-            "kw_extract",
-            "kw_list.x",
-            "load_test.x",
-            "make_grid",
-            "ri_well_test",
-            "segment_info",
-            "select_test.x",
-            "summary.x",
-        )
-    ]
-
-
-skbuild.setup(
-    name="resdata",
-    author="Equinor ASA",
-    author_email="fg_sib-scout@equinor.com",
-    description="Package for reading and writing the fortran result files from reservoir simulators",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/equinor/resdata",
-    packages=setuptools.find_packages(
-        where="python",
-        exclude=["*.tests", "*.tests.*", "tests.*", "tests", "ert.*", "ert"],
-    ),
+setup(
+    packages=find_packages(where="python", exclude=("tests",)),
     package_dir={"": "python"},
-    license="GPL-3.0",
-    platforms="any",
-    install_requires=[
-        "cwrap",
-        "numpy",
-        "pandas",
-    ],
-    setup_requires=["conan<2"],
-    entry_points={"console_scripts": utility_wrappers()},
     cmake_args=[
-        "-DRD_VERSION=" + version,
-        "-DBUILD_APPLICATIONS=" + ("ON" if sys.platform == "linux" else "OFF"),
         "-DBUILD_TESTS=OFF",
-        "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
-        "-DCMAKE_INSTALL_BINDIR=python/resdata/.bin",
-        "-DCMAKE_INSTALL_LIBDIR=python/resdata/.libs",
-        "-DCMAKE_INSTALL_INCLUDEDIR=python/resdata/.include",
+        "-DUSE_CONAN=OFF",
         # we can safely pass OSX_DEPLOYMENT_TARGET as it's ignored on
         # everything not OS X. We depend on C++11, which makes our minimum
         # supported OS X release 10.9
         "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9",
     ],
-    # skbuild's test imples develop, which is pretty obnoxious instead, use a
-    # manually integrated pytest.
-    cmdclass={"test": setuptools.command.test.test},
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Natural Language :: English",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Utilities",
-    ],
-    version=version,
 )
